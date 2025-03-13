9005f4f3b21f9b51e828ab837bc2c6850b01f1c6
DaphRz <159149963+DaphRz@users.noreply.github.com>
2025-03-13 10:59:39 -0300

    exercício 5 - casa

--- consultas.sql
+++ consultas.sql
@@ -23,6 +23,40 @@ SELECT CONCAT(fname,' ', lname) NOME, name DEPARTAMENTO
 FROM employee e, department d
 WHERE d.dept_id = e.dept_id AND superior_emp_id IS NOT NULL;
 
+# Exercício 3
 SELECT CONCAT(fname, ' ', lname) NOME
 FROM individual
 WHERE lname REGEXP '[^ry]$';   # WHERE lname NOT LIKE '%r' AND lname NOT LIKE '%y';
+
+# Exercício 4 - a
+SELECT dept_id, fname, lname, MIN(start_date)
+FROM employee
+GROUP BY dept_id;
+
+# Exercício 4 - b
+SELECT CONCAT(fname, ' ',lname) NOME, DATA.POSSE, e.dept_id
+FROM employee e, (SELECT dept_id, MIN(start_date) POSSE
+			     FROM employee
+                 GROUP BY dept_id) DATA
+WHERE e.start_date = DATA.POSSE AND e.dept_id = DATA.dept_id;
+# GROUP BY e.dept_id; - ordem CRESC
+
+# Exercício 5
+SELECT 
+	CONCAT(fname, ' ',lname) NOME, 
+	DATA.MIN, 
+    city
+FROM customer c, individual i, 
+								(SELECT cust_id, MIN(birth_date) MIN
+								FROM individual
+								GROUP BY cust_id) DATA
+WHERE i.birth_date = DATA.MIN AND i.cust_id = DATA.cust_id
+GROUP BY city;
+
+# Exercício 5
+/* SELECT CONCAT(fname, ' ',lname) NOME, MIN(birth_date), city
+FROM individual i, (SELECT cust_id, MIN(birth_date) MIN
+			     FROM individual i
+                 GROUP BY cust_id) DATA, customer c
+WHERE i.birth_date = DATA.MIN AND i.cust_id = DATA.cust_id
+ORDER BY birth_date; */
