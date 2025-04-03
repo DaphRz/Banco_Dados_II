e3e51d40134d26673035b477c02c23fe14d65d3e
DaphRz <159149963+DaphRz@users.noreply.github.com>
2025-04-03 10:37:24 -0300

    ok

--- consultas - Junção.sql
+++ consultas - Junção.sql
@@ -30,3 +30,22 @@ SELECT name branch, CONCAT(fname, ' ', lname) name
 FROM branch b JOIN customer c
 ON b.city != c.city
 JOIN individual USING(cust_id);
+
+# Junções Internas
+
+SELECT account_id, name, fed_id, birth_date
+FROM product 
+JOIN account USING (product_cd)
+JOIN customer USING (cust_id)
+JOIN individual USING(cust_id);
+
+# 5
+SELECT e.emp_id, e.fname, e.lname
+FROM employee e
+JOIN employee em ON e.superior_emp_id = em.emp_id
+WHERE e.dept_id != em.dept_id;
+
+# 5.1
+SELECT fname, lname, b.name, city, avail_balance, p.name
+FROM officer NATURAL JOIN business b NATURAL JOIN customer NATURAL JOIN account
+JOIN product p USING (product_cd);
