b89a492cbb7879eb69a3c46f9e6ed9817f2bb5b2
DaphRz <159149963+DaphRz@users.noreply.github.com>
2025-04-22 08:09:27 -0300

    index - ok

--- consultas3.sql
+++ consultas3.sql
@@ -9,3 +9,21 @@ WHERE txn_date = '2000-01-15';
 
 CREATE INDEX txn_01                  -- CREATE INDEX dept_name_idx    
 ON transaction(txn_date);            -- ON department(name);            
+
+# Exercício 2
+ALTER TABLE customer
+ADD UNIQUE fed_id_unq (fed_id);
+
+INSERT INTO customer(fed_id) VALUES(1);
+INSERT INTO customer(fed_id) VALUES(1);     -- Error Code: 1062. Duplicate entry '1' for key 'customer.fed_id_unq'	0.000 sec
+
+# Exercício 3
+ALTER TABLE account
+ADD UNIQUE cust_product_unq (cust_id,product_cd);
+
+INSERT INTO account(cust_id,product_cd,open_date) VALUES(1,"carro",now());
+INSERT INTO account(cust_id,product_cd) VALUES(1,"carro",now());
+
+# Exercício 4
+ALTER TABLE employee
+ADD UNIQUE superior_dept_unq (superior_emp_id,dept_id);    -- Error Code: 1062. Duplicate entry '1-3' for key 'employee.superior_dept_unq'	0.031 sec
