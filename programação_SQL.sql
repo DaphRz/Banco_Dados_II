46e956246ab269e1ea750028deeb226b51993022
DaphRz <159149963+DaphRz@users.noreply.github.com>
2025-05-15 10:56:54 -0300

    trigger 2 - ok

--- programação_SQL.sql
+++ programação_SQL.sql
@@ -64,7 +64,7 @@ CREATE TRIGGER CHK
 AFTER INSERT ON customer 
 FOR EACH ROW
 BEGIN
-	INSERT INTO account (cust_id, product_cd, open_date)
+    INSERT INTO account (cust_id, product_cd, open_date)
     VALUES (NEW.cust_id, 'CHK', now());
 END $$
 
@@ -75,3 +75,75 @@ SELECT account_id, product_cd, cust_id, open_date
 FROM account
 ORDER BY 1 DESC
 LIMIT 1;
+
+# Exercício 7
+
+-- Gatilho disparado quando um registro de employee é modificado
+
+USE bank;
+SHOW triggers;
+DROP TRIGGER t_DBT;
+
+CREATE TABLE log_employee_1 (
+log_id SMALLINT AUTO_INCREMENT,
+user VARCHAR(100) NOT NULL,
+fname VARCHAR(100) NOT NULL,
+lname VARCHAR(100) NOT NULL,
+date_log DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+emp_id SMALLINT(5),
+old_sup SMALLINT(5),
+new_sup SMALLINT(5),
+CONSTRAINT pk_log_employee PRIMARY KEY (log_id)
+);
+
+DELIMITER $$
+CREATE TRIGGER trg_emp_1 
+AFTER UPDATE ON employee 
+FOR EACH ROW
+BEGIN
+INSERT INTO log_employee_1(user, fname, lname, emp_id, old_sup, new_sup)
+VALUES(USER(), CONCAT(NEW.fname, " ",NEW.lname), NEW.emp_id, OLD.superior_emp_id,NEW.superior_emp_id);
+END $$
+
+UPDATE employee
+SET superior_emp_id = 2
+WHERE emp_id = 3;
+
+UPDATE employee
+SET superior_emp_id = 3
+WHERE emp_id = 1;
+
+SELECT *
+FROM log_employee_1;
+
+# Exercício 8
+
+DELIMITER $$
+
+CREATE TRIGGER t_DBT 
+BEFORE INSERT ON transaction 
+FOR EACH ROW
+BEGIN
+	DECLARE saldo_atual DOUBLE;
+		SELECT avail_balance 
+        INTO saldo_atual
+		FROM account
+		WHERE account_id = NEW.account_id;
+        
+	IF (NEW.txn_type_cd = 'DBT' AND saldo_atual < NEW.amount)
+		THEN
+		SIGNAL SQLSTATE '02000'
+		SET MESSAGE_TEXT = "Retire um valor menor que seu saldo!";
+	END IF;
+
+END $$
+
+DELIMITER ;
+
+SELECT avail_balance 
+FROM account
+WHERE account_id = 23;
+
+INSERT INTO transaction
+(txn_id ,txn_date, account_id, txn_type_cd, amount, funds_avail_date)
+VALUES (null,now(),23, 'DBT', 1600, now());
